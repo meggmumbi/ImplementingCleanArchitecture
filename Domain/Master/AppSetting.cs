6c029609d8fb850c57fa637c4e56268eafba55d2
meggmumbi <margaretmumbi16@gmail.com>
2023-04-27 18:01:13 +0300

    BASE PROJECT

---
+++ Domain/Master/AppSetting.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Master
+{
+    public class AppSetting : BaseEntity<int>
+    {
+        public string ReferenceKey { get; set; } = string.Empty;
+
+        public string Value { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public string Type { get; set; } = string.Empty;
+    }
+}
---
+++ Infrastructure/persistence/ApplicationDBContext.cs
@@ -0,0 +1,21 @@
+﻿using Application;
+using Domain.Master;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.persistence
+{
+    public class ApplicationDBContext : DbContext, IApplicationDBContext
+    {
+        public DbSet<AppSetting> appSettings { get; set; }
+
+        public Task<int> SaveChangesAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
